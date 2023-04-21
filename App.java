6fbe19a26b6c5efb69dcabbc73daac7d80622f90
JexHunter14 <121126071+JexHunter14@users.noreply.github.com>
2023-04-21 18:50:28 -0500

    Update App.java

--- App.java
+++ App.java
@@ -1,9 +1,14 @@
 package com.example;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.*;
 import javafx.stage.Stage;
@@ -17,23 +22,21 @@ public class App extends Application {
  
 private static Scene scene;
  
-private static Label FIRM;
- 
 @Override
  
 public void start(Stage stage) {
 
 stage.setTitle("Main Menu"); 
  
-Label FIRM = new Label("F.I.R.M");
- 
-FIRM.setStyle("-fx-font-size: 100px; -fx-font-family: Arial; -fx-text-fill: black;");
+Image image = new Image("FIRM.png");
+ImageView imageView = new ImageView();
+imageView.setImage(image);
  
 Button submit = new Button("Play");
  
 submit.setAlignment(Pos.CENTER);
  
-submit.setStyle("-fx-text-fill: red; -fx-background-color: black; -fx-font-size: 35px;");
+submit.setStyle("-fx-text-fill: red; -fx-background-color: black; -fx-font-size: 25px;");
  
  
 submit.setOnAction(e -> 
@@ -48,7 +51,7 @@ Button HighScore = new Button("High Scores");
  
 HighScore.setAlignment(Pos.CENTER);
  
-HighScore.setStyle("-fx-text-fill: yellow; -fx-background-color: black; -fx-font-size: 35px;");
+HighScore.setStyle("-fx-text-fill: yellow; -fx-background-color: black; -fx-font-size: 25px;");
  
  
 HighScore.setOnAction(e -> 
@@ -61,7 +64,7 @@ new HighScores(stage);
 
 Button quit = new Button("Quit");
 quit.setAlignment(Pos.CENTER);
-quit.setStyle("-fx-text-fill: white; -fx-background-color: black; -fx-font-size: 35px;");
+quit.setStyle("-fx-text-fill: white; -fx-background-color: black; -fx-font-size: 25px;");
 
 quit.setOnAction(e -> 
 {
@@ -73,14 +76,14 @@ VBox button = new VBox(submit, HighScore, quit);
 button.setAlignment(Pos.CENTER);
 button.setSpacing(20);
  
-VBox container = new VBox(FIRM, button);
+VBox container = new VBox(imageView, button);
  
 container.setPadding(new Insets(20)
 );
  
 container.setSpacing(20);
 container.setAlignment(Pos.TOP_CENTER);
-container.setStyle("-fx-background-color: cyan;");
+container.setStyle("-fx-background-color: orange;");
 
  
 scene = new Scene(container, 800, 600);
